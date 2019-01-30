f2be5f213527a80dab88bf5e2bd15a6e63c94040
Your Name <you@example.com>
2019-01-30 13:16:02 +0100

    commit

--- code/app.js
+++ code/app.js
@@ -1,13 +1,13 @@
 var app = {
 
-    init : function(){
-       // alert("Benvenuti nel BarBapapa ");
-        $.get("https://api.github.com/users/danieleleo99");
-        .done(app.onSuccess);
+    init: function () {
+        // alert("Benvenuti nel BarBapapa ");
+        $.get("https://api.github.com/users/piropiro7")
+            .done(app.onSuccess);
     },
-    onSuccess:function(data){
-    console.log(dat)
-}
+    onSuccess: function (data) {
+        console.log(data)
+    }
 };
 
 
