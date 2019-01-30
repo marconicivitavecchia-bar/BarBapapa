04bbdbe643d60d6ca3e1e9836853c441143da774
Your Name <you@example.com>
2019-01-30 13:14:05 +0100

    ciao

--- code/app.js
+++ code/app.js
@@ -1,1 +1,17 @@
-alert("Benevenuti nel nostro bar");
+var app = {
+
+    init : function(){
+       // alert("Benvenuti nel BarBapapa ");
+        $.get("https://api.github.com/users/danieleleo99");
+        .done(app.onSuccess);
+    },
+    onSuccess:function(data){
+    console.log(dat)
+}
+};
+
+
+$(document).ready(app.init);
+
+
+
